8c3ce5d429a1f42ecc61456b6bd11b491c3c915b
Miracle <miraclepatidehen@gmail.com>
2023-06-25 19:55:59 +0100

    created routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,19 @@
 import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import SignUp from './pages/SignUp'
+import Login from './pages/Login'
 
 const App = () => {
-  return <div>App</div>
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="signup" element={<SignUp />} />
+        <Route path="login" element={<Login />} />
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
 export default App
