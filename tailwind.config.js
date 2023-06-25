4d922b503086567c91ea1e6a630d96b3f970f006
Miracle <miraclepatidehen@gmail.com>
2023-06-25 19:53:02 +0100

    added color palettes

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#7848F4',
+        backgroundGrey: '#F8F8FA',
+        navyBlue: '#10107B',
+        grey600: '#7E7E7E',
+      },
+    },
   },
   plugins: [],
 }
