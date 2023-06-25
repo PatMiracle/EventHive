d76692384b13c8a91eaa704c413883e56ddd2010
Miracle <miraclepatidehen@gmail.com>
2023-06-25 19:55:08 +0100

    added data

---
+++ src/utils/data.js
@@ -0,0 +1,40 @@
+//temp event data
+const event = [
+  {
+    img: '/image3.png',
+    text: 'BestSelller Book Bootcamp -write, Market & Publish Your Book -Lucknow',
+    date: 'Saturday, March 18, 9.30PM',
+    type: 'ONLINE EVENT - Attend anywhere',
+  },
+  {
+    img: '/image2.png',
+    text: 'BestSelller Book Bootcamp -write, Market & Publish Your Book -Lucknow',
+    date: 'Saturday, March 18, 9.30PM',
+    type: 'ONLINE EVENT - Attend anywhere',
+  },
+  {
+    img: '/image2.png',
+    text: 'BestSelller Book Bootcamp -write, Market & Publish Your Book -Lucknow',
+    date: 'Saturday, March 18, 9.30PM',
+    type: 'ONLINE EVENT - Attend anywhere',
+  },
+  {
+    img: '/image2.png',
+    text: 'BestSelller Book Bootcamp -write, Market & Publish Your Book -Lucknow',
+    date: 'Saturday, March 18, 9.30PM',
+    type: 'ONLINE EVENT - Attend anywhere',
+  },
+  {
+    img: '/image1.png',
+    text: 'BestSelller Book Bootcamp -write, Market & Publish Your Book -Lucknow',
+    date: 'Saturday, March 18, 9.30PM',
+    type: 'ONLINE EVENT - Attend anywhere',
+  },
+  {
+    img: '/image3.png',
+    text: 'BestSelller Book Bootcamp -write, Market & Publish Your Book -Lucknow',
+    date: 'Saturday, March 18, 9.30PM',
+    type: 'ONLINE EVENT - Attend anywhere',
+  },
+]
+export { event }
