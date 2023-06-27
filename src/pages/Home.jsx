9ba9374b2618c8b71c8dea77bc5c739fceceb425
Miracle <miraclepatidehen@gmail.com>
2023-06-27 14:11:13 +0100

    updated home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Navbar from '../components/Navbar'
 import Footer from '../components/Footer'
 import Event from '../components/Event'
-import { cards, event } from '../utils/data'
+import { brands, cards, event } from '../utils/data'
 import {
   PiCaretLeftLight,
   PiCaretRightLight,
@@ -61,12 +61,26 @@ const Home = () => {
       <main>
         {/* upcoming events */}
         <section className="w-10/12 mx-auto py-36">
-          <div className="flex ">
+          <div className="flex items-center justify-between">
             <h2 className="text-4xl font-bold">
               Upcoming <span className="text-primary">Events</span>
             </h2>
+            <div className="flex gap-4">
+              <div className="flex items-center bg-[#efeff1] px-4 py-2 justify-between cursor-pointer w-36 rounded-md text-sm">
+                <span>Weekdays</span>
+                <PiCaretDownBold size={20} />
+              </div>
+              <div className="flex items-center bg-[#efeff1] px-4 py-2 justify-between cursor-pointer w-36 rounded-md text-sm">
+                <span>Event type</span>
+                <PiCaretDownBold size={20} />
+              </div>
+              <div className="flex items-center bg-[#efeff1] px-4 py-2 justify-between cursor-pointer w-36 rounded-md text-sm">
+                <span>Any category</span>
+                <PiCaretDownBold size={20} />
+              </div>
+            </div>
           </div>
-          <div className="grid gap-5 grid-cols-3 w-full py-8">
+          <div className="grid gap-5 grid-cols-3 w-full pt-8 pb-10">
             {event.map((data, i) => {
               return <Event key={i} {...data} />
             })}
@@ -75,14 +89,48 @@ const Home = () => {
             Load more...
           </button>
         </section>
+        {/* make event */}
+        <div className="relative before:absolute before:bg-navyBlue before:-z-10 before:h-4/5 before:w-full before:left-0 before:bottom-0 text-backgroundGrey flex pt-8 items-center pr-64 pl-20 justify-between">
+          <img src="/make_event.png" alt="" />
+          <div className="w-max">
+            <h3 className="text-4xl font-bold">Make your own Event</h3>
+            <p className="text-lg pt-4 pb-5">
+              Lorem ipsum dolor sit amet, consectetur <br />
+              adipiscing elit.
+            </p>
+            <button className="py-4 w-72 text-center text-lg bg-primary rounded-md">
+              Create Events
+            </button>
+          </div>
+        </div>
+        {/* brands */}
+        <section className="w-10/12 mx-auto text-center py-20">
+          <h2 className="text-4xl font-bold">
+            Join these <span className="text-primary">brands</span>
+          </h2>
+          <p className="max-w-lg mx-auto mt-5">
+            We've had the pleasure of working with industry-defining brands.
+            These are just some of them.
+          </p>
+          <div className="py-20">
+            <div className="grid grid-cols-5 items-center">
+              {brands.slice(0, 5).map((src, i) => {
+                return <img src={src} alt="" key={i} />
+              })}
+            </div>
+            <div className="grid grid-cols-4 items-center">
+              {brands.slice(5, 9).map((src, i) => {
+                return <img src={src} alt="" key={i} />
+              })}
+            </div>
+          </div>
+        </section>
         {/* trending colleges */}
         <section className="w-10/12 mx-auto">
-          <div className="flex ">
-            <h2 className="text-4xl font-bold">
-              Trending <span className="text-primary">Colleges</span>
-            </h2>
-          </div>
-          <div className="grid gap-5 grid-cols-3 w-full py-8">
+          <h2 className="text-4xl font-bold">
+            Trending <span className="text-primary">Colleges</span>
+          </h2>
+          <div className="grid gap-5 grid-cols-3 w-full pt-8 pb-10">
             {cards.map((data, i) => {
               return <Card key={i} {...data} />
             })}
@@ -103,9 +151,6 @@ const Home = () => {
               return <Event key={i} {...data} />
             })}
           </div>
-          <button className="bg-primary text-white rounded-md py-3 px-7 block mx-auto">
-            Load more...
-          </button>
         </section>
       </main>
       <Footer />
