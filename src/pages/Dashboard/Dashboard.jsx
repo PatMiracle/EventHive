0568049fc8bc7ab2befe37fdea74e3c525bf641e
Miracle <miraclepatidehen@gmail.com>
2023-07-03 18:19:57 +0100

    added changes

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -9,9 +9,28 @@ const Dashboard = () => {
         <div className="bg-white rounded-lg h-52 "></div>
         <div className="bg-white rounded-lg h-52 "></div>
       </section>
-      <section className="bg-white mt-10 rounded-[20px] p-8">
-        <div>
-          <h3>Weekly Revenue</h3>
+      <section className="grid gap-x-5 gap-y-10 pt-10 pb-4">
+        <div className="bg-white rounded-[20px] p-8 col-span-2">
+          <div className="flex justify-between items-center">
+            <h3>Weekly Revenue</h3>
+            <span className="py-2 px-4 bg-[#f4f7fe] rounded-lg">
+              <img src="/bar_chart.svg" alt="" />
+            </span>
+          </div>
+        </div>
+        <div className="bg-white rounded-[20px] p-8 col-span-1">
+          <div className="flex justify-between items-center">
+            <h4 className="text-lg font-bold">Your Pie Chart</h4>
+            <select name="" id="" className="text-[#687C94]">
+              <option value="monthly">Monthly</option>
+            </select>
+          </div>
+        </div>
+        <div className="bg-white rounded-[20px] p-8 col-span-1">
+          <div className="flex justify-between items-center">
+            <h4 className="text-[#A3AED0] font-medium">Daily Traffic</h4>
+            <span className="text-[#05CD99] font-bold">+2.45%</span>
+          </div>
         </div>
       </section>
     </>
