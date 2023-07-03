322d4add2988415a21491f7926a83060920e7a31
Miracle <miraclepatidehen@gmail.com>
2023-07-03 12:33:22 +0100

    updated register page

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,7 +1,30 @@
-import React, { useEffect } from 'react'
+import Navbar from '../components/Navbar'
 
 const Register = () => {
-  return <div>Register</div>
+  return (
+    <>
+      <Navbar />
+      <form className="grid gap-8 w-7/12 max-w-3xl mx-auto pt-14">
+        <h2 className="text-center">Registration</h2>
+        <div>
+          <label htmlFor="name">Your name</label>
+          <input
+            type="text"
+            className="w-full mt-1 px-[10px] py-2"
+            placeholder="Enter your name"
+          />
+        </div>
+        <div>
+          <label htmlFor="email">Your Email</label>
+          <input
+            type="text"
+            className="w-full mt-1 px-[10px] py-2"
+            placeholder="Enter your mail"
+          />
+        </div>
+      </form>
+    </>
+  )
 }
 
 export default Register
