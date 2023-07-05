50596a59f5ce2d7a7af2c2bd35d351cb85e2f735
Miracle <miraclepatidehen@gmail.com>
2023-07-05 05:53:58 +0100

    added temp data

--- src/utils/data.js
+++ src/utils/data.js
@@ -66,4 +66,37 @@ const brands = [
   '/brands/uber.png',
   '/brands/grab.png',
 ]
-export { event, cards, brands }
+//temp charts data
+const stackedColumn = [
+  { name: '17', x: 50, y: 50, z: 30 },
+  { name: '18', x: 50, y: 50, z: 30 },
+  { name: '19', x: 50, y: 50, z: 32 },
+  { name: '20', x: 44, y: 50, z: 23 },
+  { name: '21', x: 50, y: 45, z: 20 },
+  { name: '22', x: 62, y: 50, z: 29 },
+  { name: '23', x: 50, y: 50, z: 30 },
+  { name: '24', x: 50, y: 50, z: 30 },
+  { name: '25', x: 50, y: 43, z: 30 },
+]
+const piechart = [
+  {
+    name: 'Your Files',
+    value: 63,
+    color: '#775ffc',
+  },
+  {
+    name: 'System',
+    value: 25,
+    color: '#84d9fd',
+  },
+]
+const dailyTraffic = [
+  { name: '00', value: 44 },
+  { name: '04', value: 54 },
+  { name: '08', value: 54 },
+  { name: '16', value: 54 },
+  { name: '18', value: 30 },
+  { name: '18', value: 20 },
+  { name: '18', value: 54 },
+]
+export { event, cards, brands, stackedColumn, piechart, dailyTraffic }
