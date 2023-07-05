4e29a17a97cc688d4c12e8106f54e8119308f775
Miracle <miraclepatidehen@gmail.com>
2023-07-05 13:58:16 +0100

    updated

--- src/components/CollegeCard.jsx
+++ src/components/CollegeCard.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
 import { BiDotsHorizontalRounded } from 'react-icons/bi'
+import { Link } from 'react-router-dom'
 
 const Card = ({ img, college, location }) => {
   return (
-    <div className="bg-white rounded-lg shadow-lg hover:scale-[1.03] duration-300">
+    <Link
+      to="/college"
+      className="bg-white rounded-lg shadow-lg hover:scale-[1.03] duration-300"
+    >
       <img src={img} alt="" className="w-full" />
       <h3 className="px-6 py-6">{college}</h3>
       <div className="flex px-6 py-6 justify-between items-center">
@@ -12,7 +16,7 @@ const Card = ({ img, college, location }) => {
           <BiDotsHorizontalRounded size={30} />
         </div>
       </div>
-    </div>
+    </Link>
   )
 }
 
--- src/components/EventCard.jsx
+++ src/components/EventCard.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Event = ({ img, text, date, type }) => {
   return (
-    <div className="bg-white p-5 rounded-lg relative shadow-xl hover:scale-[1.03] duration-300">
+    <Link
+      to="/event"
+      className="bg-white p-5 rounded-lg relative shadow-xl hover:scale-[1.03] duration-300"
+    >
       <span className="select-none bg-white px-2 py-1 rounded-[5px] text-primary text-sm absolute top-8 left-8">
         FREE
       </span>
@@ -10,7 +14,7 @@ const Event = ({ img, text, date, type }) => {
       <p className="w-full mt-2">{text}</p>
       <p className="text-primary text-sm mt-4 mb-5">{date}</p>
       <p className="text-xs text-grey600">{type}</p>
-    </div>
+    </Link>
   )
 }
 
